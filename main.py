803d9c74a5aceb9827e2226db69242b2cced86f4
Tofu Gang <tofugangsw@gmail.com>
2021-11-16 20:38:01 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,24 @@
+__author__ = "Tofu Gang"
+__email__ = "tofugangsw@gmail.com"
+
+# from day_xx.day_xx import puzzle_x as puzzle
+
+"""
+Santa's sleigh uses a very high-precision clock to guide its movements, and the 
+clock's oscillator is regulated by stars. Unfortunately, the stars have been 
+stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve 
+all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day 
+in the Advent calendar; the second puzzle is unlocked when you complete the 
+first. Each puzzle grants one star. Good luck!
+"""
+
+################################################################################
+
+if __name__ == "__main__":
+    print("---Advent Of Code 2016---")
+    # puzzle()
+    exit(0)
+
+################################################################################
