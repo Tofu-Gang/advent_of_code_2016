6a987525cdf49e0a4faf3df8aea5cc804fb54194
Jakub Franěk <tofugangsw@gmail.com>
2022-11-10 05:52:34 +0100

    Initial commit. A function that prints a puzzle solution implemented.

---
+++ advent_of_code_2016.py
@@ -0,0 +1,28 @@
+__author__ = "Jakub Franěk"
+__email__ = "tofugangsw@gmail.com"
+
+"""
+Santa's sleigh uses a very high-precision clock to guide its movements, and the 
+clock's oscillator is regulated by stars. Unfortunately, the stars have been 
+stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve 
+all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day 
+in the Advent calendar; the second puzzle is unlocked when you complete the 
+first. Each puzzle grants one star. Good luck!
+"""
+
+# from src.day_xx import puzzle
+
+################################################################################
+
+if __name__ == "__main__":
+    """
+    Runs specified puzzles.
+    """
+
+    pass
+    # puzzle.puzzle_01()
+    # puzzle.puzzle_02()
+
+################################################################################
