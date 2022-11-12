d037afa6b5b9860299b88781231aca7167d2daa0
Jakub Franěk <tofugangsw@gmail.com>
2022-11-12 19:12:07 +0100

    Day 05: complete.

---
+++ src/day_05/puzzle.py
@@ -0,0 +1,122 @@
+__author__ = "Jakub Franěk"
+__email__ = "tofugangsw@gmail.com"
+
+"""
+--- Day 5: How About a Nice Game of Chess? ---
+
+You are faced with a security door designed by Easter Bunny engineers that seem 
+to have acquired most of their security knowledge by watching hacking movies.
+"""
+
+from typing import Generator
+from itertools import count
+from hashlib import md5
+from src.utils.utils import print_puzzle_solution
+
+DOOR_ID = "uqwqemis"
+HASH_PREFIX = "00000"
+
+
+################################################################################
+
+def md5_hash_generator() -> Generator[str, None, None]:
+    """
+    Generates an MD5 hash of the given Door ID and an increasing integer index
+    (starting with 0). Only hashes that start with a desired prefix are yielded.
+    """
+
+    for index in count():
+        md5_hash = md5(bytes(DOOR_ID + str(index), encoding="utf8")).hexdigest()
+        if md5_hash.startswith(HASH_PREFIX):
+            yield md5_hash
+
+
+################################################################################
+
+def puzzle_01() -> None:
+    """
+    The eight-character password for the door is generated one character at a
+    time by finding the MD5 hash of some Door ID (your puzzle input) and an
+    increasing integer index (starting with 0).
+
+    A hash indicates the next character in the password if its hexadecimal
+    representation starts with five zeroes. If it does, the sixth character in
+    the hash is the next character of the password.
+
+    For example, if the Door ID is abc:
+
+    -The first index which produces a hash that starts with five zeroes is
+     3231929, which we find by hashing abc3231929; the sixth character of the
+     hash, and thus the first character of the password, is 1.
+    -5017308 produces the next interesting hash, which starts with 000008f82...,
+     so the second character of the password is 8.
+    -The third time a hash starts with five zeroes is for abc5278568,
+     discovering the character f.
+
+    In this example, after continuing this search a total of eight times, the
+    password is 18f47a30.
+
+    Given the actual Door ID, what is the password?
+
+    :return: None; Answer should be 1a3099aa.
+    """
+
+    md5_hash = md5_hash_generator()
+    print_puzzle_solution("".join(next(md5_hash)[5] for _ in range(8)))
+
+
+################################################################################
+
+def puzzle_02() -> None:
+    """
+    As the door slides open, you are presented with a second door that uses a
+    slightly more inspired security mechanism. Clearly unimpressed by the last
+    version (in what movie is the password decrypted in order?!), the Easter
+    Bunny engineers have worked out a better solution.
+
+    Instead of simply filling in the password from left to right, the hash now
+    also indicates the position within the password to fill. You still look for
+    hashes that begin with five zeroes; however, now, the sixth character
+    represents the position (0-7), and the seventh character is the character to
+    put in that position.
+
+    A hash result of 000001f means that f is the second character in the
+    password. Use only the first result for each position, and ignore invalid
+    positions.
+
+    For example, if the Door ID is abc:
+
+    -The first interesting hash is from abc3231929, which produces 0000015...;
+     so, 5 goes in position 1: _5______.
+    -In the previous method, 5017308 produced an interesting hash; however, it
+     is ignored, because it specifies an invalid position (8).
+    -The second interesting hash is at index 5357525, which produces 000004e...;
+     so, e goes in position 4: _5__e___.
+
+    You almost choke on your popcorn as the final character falls into place,
+    producing the password 05ace8e3.
+
+    Given the actual Door ID and this new method, what is the password? Be extra
+    proud of your solution if it uses a cinematic "decrypting" animation.
+
+    :return: None; Answer should be 694190cd.
+    """
+
+    password = list("        ")
+    md5_hash = md5_hash_generator()
+
+    while " " in password:
+        try:
+            interesting_hash = next(md5_hash)
+            position = int(interesting_hash[5])
+            character = interesting_hash[6]
+
+            if password[position] == " ":
+                # use only the first result for each position
+                password[position] = character
+        except (ValueError, IndexError):
+            # invalid position, move on
+            pass
+    print_puzzle_solution("".join(password))
+
+################################################################################
