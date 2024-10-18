ee70a66657800acc230aa7c2478f94f83b442679
Jakub Franěk <tofugangsw@gmail.com>
2024-10-18 15:22:56 +0200

    Create main function as a test suite.

---
+++ advent_of_code_2016.py
@@ -0,0 +1,35 @@
+__author__ = "Jakub Franěk"
+__email__ = "tofugangsw@gmail.com"
+
+"""
+Santa's sleigh uses a very high-precision clock to guide its movements, and the 
+clock's oscillator is regulated by stars. Unfortunately, the stars have been 
+stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve 
+all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day 
+in the Advent calendar; the second puzzle is unlocked when you complete the 
+first. Each puzzle grants one star. Good luck!
+"""
+
+from unittest import TestCase, main
+
+# from src.day_01 import puzzle as day_01
+
+
+################################################################################
+
+class TestAdventOfCode2016(TestCase):
+
+    def test_advent_of_code_2016(self):
+        pass
+        # self.assertEqual(day_01.puzzle_01(), -1)
+        # self.assertEqual(day_01.puzzle_02(), -1)
+
+################################################################################
+
+
+if __name__ == '__main__':
+    main()
+
+################################################################################
